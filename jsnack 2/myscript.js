675d5d0c7473ea97ae06b22c69bb34a6091c4aed
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2021-12-16 17:03:01 +0100

    rename cartelle

---
+++ jsnack 2/myscript.js
@@ -0,0 +1,7 @@
+// JSnack 2
+// Fai inserire un numero, che chiameremo N, all’utente.
+// Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100.
+// Ogni volta che ne crei uno, stampalo a schermo.
+
+let N = parseInt(prompt("inserisci un numero"))
+
