7f570a8f72095896833179b1d59416b6c6c091cb
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2021-12-16 16:42:07 +0100

    somma

--- snack1/myscript.js
+++ snack1/myscript.js
@@ -3,10 +3,16 @@
 // Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando 
 // la somma degli elementi è minore di 50.
 
-let numeriArr = []
-let somma= 0;
+let numeriArr = [];
+let somma = 0;
+let i = 0;
 
 do{
-   
+   let numeri = parseInt(prompt('inserisci un numero'));
+   numeriArr.push(numeri);
+   somma += numeriArr[i];
+   i++;
 
-}while()
+}while(somma<50);
+
+document.writeln(somma);
