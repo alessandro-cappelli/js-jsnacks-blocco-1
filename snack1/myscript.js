f5428c83241cfddfbf722ea359c801abeb3637e2
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2021-12-16 15:55:14 +0100

    inizio

---
+++ snack1/myscript.js
@@ -0,0 +1,12 @@
+// JSnack 1
+// Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. 
+// Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando 
+// la somma degli elementi è minore di 50.
+
+let numeriArr = []
+let somma= 0;
+
+do{
+   
+
+}while()
