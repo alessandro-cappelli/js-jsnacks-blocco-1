a72529ab435c69eb10bbd58165d6d86cb54d7e24
alessandro-cappelli <alessandrocappelli0507@gmail.com>
2021-12-17 01:22:26 +0100

    numeri casuali

--- jsnack 2/myscript.js
+++ jsnack 2/myscript.js
@@ -4,4 +4,11 @@
 // Ogni volta che ne crei uno, stampalo a schermo.
 
 let N = parseInt(prompt("inserisci un numero"))
+let NArr = []
 
+for(let i=0; i<N; i++){
+    let randN = Math.floor(Math.random() *100 + 1);
+    NArr.push(randN);
+    console.log(NArr)
+}
+document.write(NArr)
